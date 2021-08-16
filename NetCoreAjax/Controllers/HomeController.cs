8bd23f7491ec4bb5a7d978aef4643441079302b2
omar3000 <ofa1995f14@hotmail.com>
2021-08-16 04:07:16 -0500

    first commit

---
+++ NetCoreAjax/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NetEjercicio.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace NetEjercicio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            var jsonUsers = JsonConvert.SerializeObject(UserAction.GetAll());
+            return Json(jsonUsers);
+        }
+        [HttpPost]
+        public IActionResult Add(User user)
+        {
+            UserAction.Add(user);
+            var jsonUser = JsonConvert.SerializeObject(user);
+            return Json(jsonUser);
+        }
+
+        public IActionResult GetById(int UserId)
+        {
+            var foundUser = UserAction.GetById(UserId);
+            var jsonUser = JsonConvert.SerializeObject(foundUser);
+            return Json(jsonUser);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ NetCoreAjax/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetEjercicio.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public static class UserAction
+    {
+        private static List<User> users = new List<User>()
+        {
+            new User {Id = 1 , Name="nombre1"},
+            new User {Id = 2 , Name="nombre2"}
+        };
+
+        public static List<User> GetAll()
+        {
+            return users;
+        }
+
+        public static User GetById(int id)
+        {
+            return users.FirstOrDefault(I => I.Id == id);
+        }
+
+        public static void Add(User user)
+        {
+            users.Add(user);
+        }
+    }
+}
